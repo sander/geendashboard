bcfced3e0019390b08e59fdc06e3c6716b6d9b5a
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2013-07-07 02:00:43 +0200

    Display involved people with project details

--- geendashboard/settings.py
+++ geendashboard/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'projects',
     'wunderlist',
+    'profiles',
 )
 
 MIDDLEWARE_CLASSES = (
---
+++ profiles/templatetags/gravatar.py
@@ -0,0 +1,32 @@
+from django import template
+import hashlib
+import urllib.parse
+
+register = template.Library()
+
+class GravatarUrlNode(template.Node):
+  def __init__(self, email):
+    self.email = template.Variable(email)
+
+  def render(self, context):
+    try:
+      email = self.email.resolve(context)
+    except template.VariableDoesNotExist:
+      return ''
+
+    default = ''
+    size = 32
+
+    return 'http://www.gravatar.com/avatar/%s?%s' % (
+      hashlib.md5(email.lower().encode('utf-8')).hexdigest(),
+      urllib.parse.urlencode({ 'd': default, 's': str(size) })
+    )
+
+@register.tag
+def gravatar_url(parser, token):
+  try:
+    tag_name, email = token.split_contents()
+  except ValueError:
+    raise(template.TemplateSyntaxError, '%r tag requires an email address argument' % token)
+
+  return GravatarUrlNode(email)

b1748c79b7d6f0402ad3f77678f8511e8c359b5e
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2013-07-21 13:51:25 +0200

    Create Project model and views

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,49 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find params[:id]
+  end
+
+  def edit
+    @project = Project.find params[:id]
+  end
+
+  def update
+    @project = Project.find params[:id]
+
+    if @project.update project_params
+      redirect_to @project
+    else
+      render 'edit'
+    end
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new project_params
+
+    if @project.save
+      redirect_to @project
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @project = Project.find params[:id]
+    @project.destroy
+
+    redirect_to projects_path
+  end
+
+  private
+    def project_params
+      params.require(:project).permit :name, :goal, :visible, :active
+    end
+end

0ed6da4d2e4c18e6412571e1ecabac9458761ce5
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2013-07-21 13:14:24 +0200

    Initial commit: standard Rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Geendashboard::Application.config.secret_key_base = '2f9321b520b7697dccc60b9e72100193abe11b29916c15b3c52cdb89d2a92ad24d5c1454809b1d64acc62f3eff614c8a1b22b181cf366014a0fe9d40f7019dc2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Geendashboard::Application.config.session_store :cookie_store, key: '_geendashboard_session'

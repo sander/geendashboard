c6685e661511256c942e4dc7445785a1ca715e79
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2013-07-23 16:42:19 +0200

    Remove Project show

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,10 +6,6 @@ class ProjectsController < ApplicationController
     end
   end
 
-  def show
-    @project = Project.find params[:id]
-  end
-
   def edit
     @project = Project.find params[:id]
   end
